5bd59341195e931dac89d5e5b2d0af597797240e
Soham Sinha <Sohamkundan@gmail.com>
2024-08-16 13:15:02 +0530

    Final Working GUI
    Working GUI with all functionality.
    Working Game.

--- ttt.py
+++ ttt.py
@@ -1,24 +1,30 @@
 import numpy as np
-import ai
+import tkinter as tk
 
 human_turn = True
-human_won = False
-bot_won = False
 no_of_turns = 0
 
 # Creating the game board
 game_board = np.zeros((3,3), int)
 
 
+root = tk.Tk()
+root.geometry("450x350")
+root.title("Tic Tac Toe")
+
+turn_label = tk.StringVar()
+turn_label.set("Your Turn")
+label = tk.Label(root, textvariable=turn_label)
+label.grid()
+label.place(x=200, y=5)
+
 # Make Move
 def make_move(row, column, game_board, turn):
     if game_board[row][column] == 1 or game_board[row][column] == 2:
         return ""
     else:
-        if turn:
-            game_board[row][column] = 1
-        else:
-            game_board[row][column] = 2
+        game_board[row][column] = 1 if turn else 2
+        return True
 
 # Check if Human (Player 1) has won, returns true if they have
 def check_human_state(game_board):
@@ -64,41 +70,109 @@ def check_bot_state(game_board):
         else:
             return False
 
-# Game Loop
-def Main():
-    global human_turn
-    global no_of_turns
+# AI Move
+'''
+Conditional Hierarchy for moving:
+1. Play moves that make the bot win
+2. Block the Opponent's Win
+3. Play the Center
+4. Random (First move in the list of all possible moves)
+'''
+def ai_move():
     global game_board
-    while True:
-        # When to end
-        if check_human_state(game_board):
-            print("Human (Player 1) Won")
-            break
-        elif check_bot_state(game_board):
-            print("Bot (Player 2) Won")
-            break
-        elif no_of_turns == 9:
-            print("Tie")
-            break
+    game = np.copy(game_board)
+    possible_moves = []
+
+    # Look for possible moves
+    for r in range(3):
+        for c in range(3):
+            if game_board[r][c] == 0:
+                possible_moves.append((r, c))
+
+    for i in possible_moves:
+        game[i[0]][i[1]] = 2
+        # Play the move which makes the bot win
+        if check_bot_state(game):
+            game_board[i[0]][i[1]] = 2
+            return
+        # If center is free, play there
+        elif (1, 1) in possible_moves:
+            game_board[1][1] = 2
+            return
         else:
-            if human_turn:
-                r = (int(input("Player 1 which row do you want to place your piece in: ")) - 1)
-                c = (int(input("Player 1 which column do you want to place your piece in: ")) - 1)
-                # If another piece has been played at that spot don't allow the player to play there
-                if make_move(r, c, game_board, human_turn) == "":
-                    print("\nCan't place here. Please enter another valid spot.")
-                    print(game_board)
-                else:
-                    make_move(r, c, game_board, human_turn)
-                    print(game_board)
-                    human_turn = False
-                    no_of_turns += 1
-
+            game = np.copy(game_board)
+    for i in possible_moves:
+        game = np.copy(game_board)
+        
+        for p in possible_moves:
+            game[p[0]][p[1]] = 1
+            # play the move which would've made the opponent win
+            if check_human_state(game):
+                game_board[p[0]][p[1]] = 2
+                return
             else:
-                ai.ai_move(game_board)
-                print(game_board)
-                human_turn = True
-                no_of_turns += 1
+                game = np.copy(game_board)
+    # Play first possible move if none are better than the others
+    game_board[possible_moves[0][0]][possible_moves[0][1]] = 2
 
-if __name__ == "__main__":
-    Main()
+
+# Button Click Function
+def button_click(button, r, c):
+    global game_board, human_turn, no_of_turns
+    if make_move(r, c, game_board, human_turn) == "":
+        turn_label.set("Can't place here. Please enter another valid spot.")
+        return
+
+    # Update the board with the player's move
+    button.config(text="X")
+    no_of_turns += 1
+
+    # Check if the player has won after their move
+    if check_human_state(game_board):
+        turn_label.set("Human (Player 1) Won!")
+        root.after(500, root.destroy)  # Close the window after a short delay
+        return
+
+    # Check if it's a draw
+    if no_of_turns == 9:
+        turn_label.set("It's a Tie!")
+        root.after(500, root.destroy)  # Close the window after a short delay
+        return
+
+    # AI makes a move
+    human_turn = False
+    ai_move()
+
+    # Update the board UI after AI move
+    for i in range(3):
+        for j in range(3):
+            if game_board[i][j] == 2:
+                buttons[i][j].config(text="O")
+
+    no_of_turns += 1
+
+    # Check if the AI has won after its move
+    if check_bot_state(game_board):
+        turn_label.set("Bot (Player 2) Won!")
+        root.after(500, root.destroy)  # Close the window after a short delay
+        return
+
+    # Switch back to human turn
+    human_turn = True
+    turn_label.set("Your Turn")
+                
+
+# 2D array of buttons
+buttons = []
+for i in range(3):
+    row_buttons = []
+    for j in range(3):
+        button = tk.Button(root, text="", height=4, width=8, 
+                           command=lambda r=i, c=j: button_click(buttons[r][c], r, c))
+        button.grid(row=i, column=j)
+        button.place(x=30 + 130 * j, y=50 + 100 * i)
+        row_buttons.append(button)
+    buttons.append(row_buttons)
+
+
+root.mainloop()
